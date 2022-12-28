720ffdb91290e80f6718c8a630e737bb75ad3415
Alabi Temitope <t.banji@rocketmail.com>
2022-12-28 04:12:04 +0100

    frontend implementation text-davinci-003

---
+++ server/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+/* 
+    REST API,
+    The difference between GET and POST is there is limited data
+    that we can get from the request method when we make use of GET but
+    we can get much more data from POST request method because it has body field
+
+*/
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from Temicodex'
+    })
+});
+
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
+            max_tokens:3000,
+            top_p:1,
+            frequency_penalty:0.5,
+            presence_penalty:0,
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.error('server error',error);
+        res.status(500).send({
+            message: 'Server error',
+            error})
+    }
+});
+
+
+app.listen(5000, () => { console.log('Server is listening on port http://localhost:5000')})
+
