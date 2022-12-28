b373f62b990f066c278a7dfaefb11765b513a7da
Alabi Temitope <t.banji@rocketmail.com>
2022-12-28 04:46:50 +0100

    added env file for backend

--- client/script.js
+++ client/script.js
@@ -15,7 +15,7 @@ function loader(element) {
   loadInterval = setInterval(() => {
     element.textContent += '.';
 
-    if (element.textContent === '....') {
+    if (element.textContext === '....') {
       element.textContent = '';
     }
   }, 300);
@@ -86,7 +86,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   //  fetach data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://temicodex.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json',
   },
